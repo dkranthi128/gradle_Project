0716b0fbaebc0c88b0a7bf2f6fc6573605493838
kranthi <kranthidevavarapu@gmail.com>
2018-04-29 01:52:14 -0400

    yyy

---
+++ src/main/java/demo/helloGradle.java
@@ -0,0 +1,9 @@
+package demo;
+import org.joda.time.LocalTime;
+class helloGradle{
+	public static void main(String...args){
+		System.out.println("Hello world Gradle");
+		LocalTime time = new LocalTime();
+		System.out.println("LocalTime:"+time);
+}
+}
---
+++ src/test/java/demoTest/helloWorld.java
@@ -0,0 +1,14 @@
+package demoTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class helloWorld {
+
+    @Test
+    public void test1(){
+		assert 1==1;
+		System.out.println("Test Passed!!");
+    }
+
+}
